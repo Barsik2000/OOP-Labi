fbc3999eff2b3289243d72bff7a32dcc19861589
Barsik2000 <57293155+Barsik2000@users.noreply.github.com>
2021-10-23 19:04:11 +0300

    Add files via upload
    Lab 3 - A*

---
+++ Lab 3 - A star/src/Location.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    public boolean equals(Object e){
+        if (this == e) return true;
+
+        if(e == null || getClass() != e.getClass()){
+            return false;
+        }
+
+        Location location = (Location) e;
+        return xCoord == location.xCoord && yCoord == location.yCoord;
+    }
+
+    public int Hashcode(){
+        return Objects.hash(xCoord, yCoord);
+    }
+
+}
